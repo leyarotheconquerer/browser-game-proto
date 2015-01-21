ee29a4517d9801ebf20ec26d861f8dbf9389f7cb
leyarotheconquerer <HazenJohnson@letu.edu>
2015-01-21 13:55:00 -0600

    Commit all the changes at once and rush to class.

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+	public Transform PlayerTransform;
+	public Vector3 Follow;
+
+	void Start () {
+	}
+
+	void Update () {
+		transform.position = PlayerTransform.position + Follow;
+		transform.LookAt(PlayerTransform.position);
+	}
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public float TurnSpeed = 0.08f;
+	public float MoveSpeed = 0.5f;
+	public float JumpSpeed = 5.0f;
+	
+	float turnAmount;
+	float moveAmount;
+
+	bool jump;
+
+	void Start () {
+		print (transform.forward);
+	}
+
+	void FixedUpdate() {
+		Vector3 facing = transform.forward.normalized;
+		Vector3 left = Vector3.Cross(facing, Vector3.up);
+
+		transform.position = transform.position + facing * MoveSpeed * moveAmount;
+		transform.forward = (left * turnAmount * TurnSpeed + facing).normalized;
+
+		if(jump)
+		{
+			rigidbody.AddForce(Vector3.up * JumpSpeed);
+		}
+	}
+
+	void Update () {
+		turnAmount = Input.GetAxis("Horizontal");
+		moveAmount = Input.GetAxis("Vertical");
+
+		jump = Input.GetButtonDown("Jump");
+	}
+}
